80b4d7609db5144a6f0fb004376b776d19cabe59
CodeforLiberty <sswell@live.com>
2016-11-21 16:28:18 -0500

    config changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://user:user@ds159527.mlab.com:59527/shooping-list' :
-                            'mongodb://Dev:Dev@ds159527.mlab.com:59527/shooping-list');
+                        global.DATABASE_URL ||
+                        (process.env.NODE_ENV === 'production' ?
+                             'mongodb://user:user@ds159527.mlab.com:59527/shooping-list' :
+                             'mongodb://Dev:Dev@ds159527.mlab.com:59527/shooping-list');
 exports.PORT = process.env.PORT || 8080;
