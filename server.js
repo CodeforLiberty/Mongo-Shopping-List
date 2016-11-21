b9bb2e59191eb57bc25f97d8ecd025fb5b7f5c27
numbergames <bfletch@gmail.com>
2016-11-21 17:21:22 -0500

    Add delete functionality

--- server.js
+++ server.js
@@ -10,56 +10,74 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/items', function(req, res) {
-    Item.find(function(err, items) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        res.json(items);
-    });
+  Item.find(function(err, items) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      });
+    }
+    console.log(items);
+    res.json(items);
+  });
 });
 
 app.post('/items', function(req, res) {
-    Item.create({
-        name: req.body.name
-    }, function(err, item) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        res.status(201).json(item);
-    });
+  Item.create({name: req.body.name}, function(err, item) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      });
+    }
+    res.status(201).json(item);
+  });
 });
 
-app.use('*', function(req, res) {
-    res.status(404).json({
+app.delete('/items/:id', function (req, res) {
+  var query = { _id: req.params.id };
+  Item.findOneAndRemove(query, function (err, item) {
+    if (err) {
+      return res.status(404).json({
         message: 'Not Found'
-    });
+      });
+    }
+    console.log(item);
+    res.status(200).json(item);
+  });
+})
+
+// app.put('/items', function (req, res) {
+//   var update = {$set: {name: name}}
+//   Item.findOneAndUpdate({name: req.name}, )
+//
+// });
+
+app.use('*', function(req, res) {
+  res.status(404).json({
+    message: 'Not Found'
+  });
 });
 
 var runServer = function(callback) {
-    mongoose.connect(config.DATABASE_URL, function(err) {
-        if (err && callback) {
-            return callback(err);
-        }
+  mongoose.connect(config.DATABASE_URL, function(err) {
+    if (err && callback) {
+      return callback(err);
+    }
 
-        app.listen(config.PORT, function() {
-            console.log('Listening on localhost:' + config.PORT);
-            if (callback) {
-                callback();
-            }
-        });
+    app.listen(config.PORT, function() {
+      console.log('Listening on localhost:' + config.PORT);
+      if (callback) {
+        callback();
+      }
     });
+  });
 };
 
 if (require.main === module) {
-    runServer(function(err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+  runServer(function(err) {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 exports.app = app;
--- test/test-server.js
+++ test/test-server.js
@@ -12,39 +12,39 @@ var app = server.app;
 chai.use(chaiHttp);
 
 describe('Shopping List', function() {
-    before(function(done) {
-        server.runServer(function() {
-            Item.create({name: 'Broad beans'},
-                {name: 'Tomatoes'},
-                {name: 'Peppers'}, function() {
-                    done();
-                });
-        });
-    });
-    it('should return an array of objects on GET', function(done) {
-        chai.request(app)
-        .get('/items')
-        .end(function(err, res) {
-                console.log(res.body);
-                should.equal(err, null);
-                res.should.have.status(200);
-                res.should.be.json;
-                // res.body.should.be.a('array');
-                // res.body.should.have.all.keys('name', 'id');
-                // res.body[0].should.be.an('object');
-                // res.body[0].name.should.be.a('string');
-                // res.body[0].id.should.be.a('number');
-                // res.body[0].name.should.equal('Broad beans');
-                // res.body[1].name.should.equal('Tomatoes');
-                // res.body[2].name.should.equal('Peppers');
-                done();
-            });
-    });
-
-
-after(function(done) {
-    Item.remove(function() {
+  before(function(done) {
+    server.runServer(function() {
+      Item.create({name: 'Broad beans'},
+      {name: 'Tomatoes'},
+      {name: 'Peppers'}, function() {
         done();
+      });
     });
-});
+  });
+  it('should return an array of objects on GET', function(done) {
+    chai.request(app)
+    .get('/items')
+    .end(function(err, res) {
+      console.log(res.body);
+      should.equal(err, null);
+      res.should.have.status(200);
+      res.should.be.json;
+      // res.body.should.be.a('array');
+      // res.body.should.have.all.keys('name', 'id');
+      // res.body[0].should.be.an('object');
+      // res.body[0].name.should.be.a('string');
+      // res.body[0].id.should.be.a('number');
+      // res.body[0].name.should.equal('Broad beans');
+      // res.body[1].name.should.equal('Tomatoes');
+      // res.body[2].name.should.equal('Peppers');
+      done();
+    });
+  });
+
+
+  after(function(done) {
+    Item.remove(function() {
+      done();
+    });
+  });
 });
