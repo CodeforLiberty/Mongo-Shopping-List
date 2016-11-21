148fdf4ae8f2227fbab491de98b40a7f598eb415
numbergames <bfletch@gmail.com>
2016-11-21 15:14:27 -0500

    Basic functionality

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://<user>:<user>@ds159527.mlab.com:59527/shooping-list' :
-                            'mongodb://<Dev>:<Dev>@ds159527.mlab.com:59527/shooping-list');
-exports.PORT = process.env.PORT || 59527;
-
-PORT=59527
+                            'mongodb://user:user@ds159527.mlab.com:59527/shooping-list' :
+                            'mongodb://Dev:Dev@ds159527.mlab.com:59527/shooping-list');
+exports.PORT = process.env.PORT || 8080;
